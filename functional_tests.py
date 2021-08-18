3b2370054f1e13692e22ba9d5366833a6c75f309
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-08-18 16:41:46 +0530

    pyblog website

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,6 @@
+"""
+Functional tests are used to test the application from the point of user.
+"""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -5,7 +8,13 @@ import time
 
 
 class HomePageTest(unittest.TestCase):
+    """
+    Class to write the unittests for the every particular function.
+    """
     def setUp(self):
+        """
+        Setup method to set the browser on and to login into the application to check the remaining pages.
+        """
         self.browser = webdriver.Chrome(
             executable_path="S:\material\chromedriver_win32\chromedriver.exe"
         )
@@ -18,6 +27,9 @@ class HomePageTest(unittest.TestCase):
         submit.send_keys(Keys.RETURN)
 
     def tearDown(self):
+        """
+        TearDown method to shut the browser after completing the testing.
+        """
         time.sleep(5)
         self.browser.quit()
 
@@ -28,9 +40,11 @@ class HomePageTest(unittest.TestCase):
         self.assertIn("pyblog", self.browser.title)
 
     def test_topics_page(self):
+        # to check whether the topics page is working.
         self.browser.get("http://localhost:8000/topics/")
 
     def test_topic_detail_page(self):
+        # to check whether the topic detail page is working.
         self.browser.get('http://localhost:8000/topics/1/1/')
 
 
